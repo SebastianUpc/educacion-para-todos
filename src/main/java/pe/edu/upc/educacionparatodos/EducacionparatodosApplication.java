5866295dabd4d3a87a14fe56f558ef0e1e4d3c25
Sebastián Rojas <186363794+SebastianUpc@users.noreply.github.com>
2026-09-13 19:16:05 -0500

    chore: proyecto base generado con Spring Initializr

---
+++ src/main/java/pe/edu/upc/educacionparatodos/EducacionparatodosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.educacionparatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducacionparatodosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducacionparatodosApplication.class, args);
+    }
+
+}
